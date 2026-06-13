cb9d0d55edaf7e2f1d07e5294391eb28c9296966
Johan Zandstra <johan@dolphiq.nl>
2026-06-14 00:13:35 +0200

    Stabilise Craft 4: fix composer + add phpstan/codeception tooling

---
+++ tests/unit/SanityTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace dolphiq\redirect\tests\unit;
+
+use Codeception\Test\Unit;
+
+/**
+ * Proves the test runner is wired up correctly.
+ */
+class SanityTest extends Unit
+{
+    public function testHarnessRuns(): void
+    {
+        $this->assertTrue(true);
+    }
+}
